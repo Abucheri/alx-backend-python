fffe108769caed60aa396f5742d54cc50ab2e187
O'Brien <abucheriobrien@gmail.com>
2024-05-13 01:29:39 +0300

    Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -80,3 +80,19 @@ class TestGithubOrgClient(unittest.TestCase):
 
         # Assert that get_json was called exactly once
         mock_get_json.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test GithubOrgClient.has_license method."""
+
+        # Create an instance of GithubOrgClient
+        test_client = GithubOrgClient("google")
+
+        # Call the method under test
+        result = test_client.has_license(repo, license_key)
+
+        # Assert that the return value matches the expected result
+        self.assertEqual(result, expected)
